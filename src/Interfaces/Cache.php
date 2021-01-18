5cf2fcc2cf263d6f2866a1b31c7f9ae0fd2c7e4b
Vladimir Chetalbashev <vladimir.chatalbashev@jiminny.com>
2021-01-18 14:48:25 +0200

    prep merge request

--- src/Interfaces/Cache.php
+++ src/Interfaces/Cache.php
@@ -4,6 +4,6 @@ namespace ProsperWorks\Interfaces;
 
 interface Cache
 {
-    public function get($key): mixed;
+    public function get($key):mixed;
     public function save(string $key, mixed $data, int $lifetime = null);
 }
